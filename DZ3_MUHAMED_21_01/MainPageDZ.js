ba8b101f07dbad6335ac522c901d188cda819e5d
Хихиза Мухамед <119803043+hihizamuhamed@users.noreply.github.com>
2023-03-29 12:50:04 +0600

    Add files via upload

---
+++ DZ3_MUHAMED_21_01/MainPageDZ.js
@@ -0,0 +1,13 @@
+
+
+function App(){
+    return(
+        <div className={"app"}>
+            <div style={{display:"flex"}}>
+                <button>PLUS</button>
+                <button>MINUS</button>
+            </div>
+        </div>
+    )
+}
+export default App
